1e89e26a09f947fce22019c82aa6f6fd193e6cac
renanbazinin <renanbazinin2@gmail.com>
2020-04-25 15:21:21 +0300

    Add files via upload
    John Horton Conway's game of life in C# console. on 8 April 2020, Conway got the symptoms of COVID-19. On 11 April, he died. I have just begun to make this when this happened. This is my first project since 2017 due to drafting to the army

---
+++ ConsoleApplication2/Cell.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Cell
+    {
+        private bool isAlive;
+        private int rowNum;
+        private int colNum;
+
+        public Cell (bool isAlive, int rowNum, int colNum)
+        {
+            this.isAlive = isAlive;
+            this.rowNum = rowNum;
+            this.colNum = colNum;
+        }
+
+        public void ToggleStatus()
+        {
+            isAlive = (isAlive)?  false :  true;
+        }
+
+        public void SetNewStatus(bool alive)
+        {
+            isAlive = alive;
+        }
+        public void Resurrect()
+        {
+            isAlive = true;
+        }
+
+
+        public bool IsAlive()
+        {
+            return isAlive;
+        }
+
+        public int GetRow()
+        {
+            return rowNum;
+        }
+
+        public int GetCol()
+        {
+            return colNum;
+        }
+    }
+}
